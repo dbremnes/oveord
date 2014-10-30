3591ca9da27f93c72f24ca1e97dca700db76d661
Sigrun <slindaas@gmail.com>
2014-10-30 12:16:51 +0100

    Første test

---
+++ PedagogiskSpill/src/Hovedklasse.java
@@ -0,0 +1,29 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+public class Hovedklasse extends JFrame
+{
+    public static void main (String[] args)
+    {
+    	//Programmet starter med å vente på input fra bruker om rolle
+        String [] verdier = {"Elev", "Lærer"};
+        JOptionPane.showInputDialog(null, "Velg rolle", "Elev eller lærer?", JOptionPane.INFORMATION_MESSAGE, null, verdier, verdier[0] );
+        
+        //Hvis rollevalg er lærer
+        //<start med å lese inn fra input/fil>
+        //<metode?>
+        
+        //Hvis ikke
+        //<åpner hovedvindu med oppgavesamling>
+        //OBS! Hvordan linker jeg "OK"-knapp sammen med åpning av hovedvindu?
+                
+        //Konstruktør. Lager og viser fram hovedvindu
+    	Hovedvindu vindu = new Hovedvindu();
+        vindu.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        vindu.setSize(1000, 800);
+        vindu.setVisible(true);
+
+        
+    }
+}
---
+++ PedagogiskSpill/src/Hovedvindu.java
@@ -0,0 +1,77 @@
+import javax.swing.*; 					  //<importerer n�dvendige pakker>
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+
+public class Hovedvindu extends JFrame implements ActionListener  //deklarerer datafelter for knapper og tekstomr�de
+{
+    public static final int MAXANTALL = 10;
+    private JButton kurs, student, oversikt, fag, avslutt;
+    private JTextField utskriftsområde;
+
+    public Hovedvindu()  		//konstrukt�r uten parametre
+    {
+        super("Øveord");				  		//super gj�r at denne subklassens konstrukt�r arver egenskapene til java-klassen Object.
+        kurs = new JButton("Kurs"+ "\n"); 		//oppretter knappene Kurs, Student, Oversikt og Avslutt
+        kurs.addActionListener(this);
+        student = new JButton("Student\t\t");
+        student.addActionListener(this);
+        oversikt = new JButton("Oversikt\t\t");
+        oversikt.addActionListener(this);
+        fag = new JButton("Fag\t\t");
+        fag.addActionListener(this);
+        avslutt = new JButton("Avslutt\t\t");
+        avslutt.addActionListener(this);
+        utskriftsområde = new JTextField (12);
+        utskriftsområde.setSize( 300, 300 ); 	//kaller p� metoden setSize og setter st�rrelsen p� vinduet.
+        utskriftsområde.addActionListener(this);
+
+        Container c = getContentPane(); 		//oppretter vinduets komponent-"container"
+        c.setBackground(Color.WHITE);
+        c.setLayout(new FlowLayout());  		//setter komponentene i "containeren" i riktig rekkef�lge
+        c.add(kurs);
+        c.add(student);
+        c.add(oversikt);
+        c.add(fag);
+        c.add(avslutt);
+    }
+
+
+    public void startStudent() 		  //her hentes Studentklassen.
+    {
+        Studentvindu vindu = new Studentvindu();
+		vindu.setSize( 700, 630 );
+		vindu.setVisible( true );
+    }
+
+ 
+    public void actionPerformed(ActionEvent e)  //Ved trykk kalles metodene opp.
+    {
+    if (e.getSource() == kurs)
+        startKurs();
+    else if (e.getSource() == student)
+        startStudent();
+    else if (e.getSource() == oversikt)
+        startOversiktsplan();
+    else if (e.getSource() == fag)
+        startFag();
+    }
+
+
+	private void startFag() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private void startOversiktsplan() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private void startKurs() {
+		// TODO Auto-generated method stub
+		
+	}
+}
