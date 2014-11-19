578c08ce3551ce1556e0cb423570bfad2653194a
dbremnes <dbremnes@yahoo.no>
2014-11-19 18:40:34 +0100

    Update oppgavetype_input.java

--- PedagogiskSpill/src/oppgavetype_input.java
+++ PedagogiskSpill/src/oppgavetype_input.java
@@ -8,64 +8,64 @@ public class oppgavetype_input {
 	private String oppgaveOrd;
 	private String oppgaveSvar;
 	private int nivaa;
-	private boolean besvart; // True når spelar har besvart oppgåva (er ferdig)
+	private boolean besvart; // True nÃ¥r spelar har besvart oppgÃ¥va (er ferdig)
 	private Panel grensesnitt;
-	
-	
+
+
 	public oppgavetype_input(String oppgaveOrd, String oppgaveSvar, int nivaa) {
 		/*
-		 * Konstruktøren. Initialiserer egenskapane til objektet. 
+		 * KonstruktÃ¸ren. Initialiserer egenskapane til objektet.
 		 */
 		super();
 		this.oppgaveOrd = oppgaveOrd;
 		this.oppgaveSvar = oppgaveSvar;
 		this.nivaa = nivaa;
-		this.setBesvart(true);
+		besvart=true;//skal stÃ¥ this.setBesvart(true)
 	}
-	
+
 	public Panel visOppgave(){
-		
+
 		/*  Inspirert av: http://cs.nyu.edu/~yap/classes/visual/03s/lect/l7/
-		 * Ideen her, som kanskje ikkje er smart, er at denne metoden returnerer eit panel. 
-		 * ut frå tanken om at det er eit element som kan puttast inn i eit vindu. 
-		 */ 
-		grensesnitt.add( this.visOppgaveSporsmaalet() ); // Vi legg til spørsmåldelen.
-		grensesnitt.add( this.visOppgaveSvarInputPanel() ); // Og svardelen...
-		grensesnitt.add( this.visSvarPaaOppgavePanel() ); // Og send-svar-delen. 
-		return grensesnitt; 
-	}
-	
-	private Panel visOppgaveSporsmaalet(){
-		Panel oppgaveSporsmaalPanelet = null; // Må finne riktig type panel. 
-		/*
-		 * Intern metode for å lage layouten for spørsmålet, slik det blir presentert.
-		 * Her blir det laga fleire panel, som blir organisert via ulike layouts. 
+		 * Ideen her, som kanskje ikkje er smart, er at denne metoden returnerer eit panel.
+		 * ut frÃ¥ tanken om at det er eit element som kan puttast inn i eit vindu.
 		 */
-		return oppgaveSporsmaalPanelet; 
+		grensesnitt.add( this.visOppgaveSporsmaalet() ); // Vi legg til spÃ¸rsmÃ¥ldelen.
+		grensesnitt.add( this.visOppgaveSvarInputPanel() ); // Og svardelen...
+		grensesnitt.add( this.visSvarPaaOppgavePanel() ); // Og send-svar-delen.
+		return grensesnitt;
 	}
-	
-	private Panel visOppgaveSvarInputPanel(){
-		Panel oppgaveSvarInputPanelet = null; // Må finne riktig type panel. 
+
+	private Panel visOppgaveSporsmaalet(){
+		Panel oppgaveSporsmaalPanelet = null; // MÃ¥ finne riktig type panel.
 		/*
-		 * Intern metode for å lage layouten for SVAR-input.
-		 * Her blir det laga fleire panel, som blir organisert via ulike layouts. 
-		 *	Det vil nok i versjon 1 være ein JOptionPane.showInputDialog(), berre for å få inn data enkelt
+		 * Intern metode for Ã¥ lage layouten for spÃ¸rsmÃ¥let, slik det blir presentert.
+		 * Her blir det laga fleire panel, som blir organisert via ulike layouts.
+		 */
+		return oppgaveSporsmaalPanelet;
+	}
+
+	private Panel visOppgaveSvarInputPanel(){
+		Panel oppgaveSvarInputPanelet = null; // MÃ¥ finne riktig type panel.
+		/*
+		 * Intern metode for Ã¥ lage layouten for SVAR-input.
+		 * Her blir det laga fleire panel, som blir organisert via ulike layouts.
+		 *	Det vil nok i versjon 1 vÃ¦re ein JOptionPane.showInputDialog(), berre for Ã¥ fÃ¥ inn data enkelt
 		 */
 		return oppgaveSvarInputPanelet;
-		
+
 	}
 	private Panel visSvarPaaOppgavePanel(){
-		Panel sendSvarPanelet = null; // Må finne riktig type panel. 
+		Panel sendSvarPanelet = null; // MÃ¥ finne riktig type panel.
 		/*
-		 * Innheld ein "Send-svar" button, som har eventlistener på å setje oppgåva "besvart"
-		 * Overflødig i versjon 1 om en brukar JOptionPane.showInputDialog() til svaret. 
+		 * Innheld ein "Send-svar" button, som har eventlistener pÃ¥ Ã¥ setje oppgÃ¥va "besvart"
+		 * OverflÃ¸dig i versjon 1 om en brukar JOptionPane.showInputDialog() til svaret.
 		 */
 		return sendSvarPanelet;
-		
+
 	}
 
 	public boolean getBesvart() {
 		return besvart;
 	}
-	
+
 }
