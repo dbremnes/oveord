e033fa42f3a9a7d1f105b7a2b5a26796f3a14d2e
dbremnes <dbremnes@yahoo.no>
2014-11-26 09:28:46 +0100

    med riktig meny valg

--- PedagogiskSpill/src/Hovedvindu.java
+++ PedagogiskSpill/src/Hovedvindu.java
@@ -6,41 +6,40 @@ import java.io.IOException;
 public class Hovedvindu extends JFrame implements ActionListener  //deklarerer datafelter for knapper og tekstomr?de
 {
     public static final int MAXANTALL = 10;
-    private JButton kurs, student, oversikt, fag, avslutt;
-    private JTextField utskriftsområde;
+    private JButton laerer, elev, besvart, avslutt;
+    private JTextField oppgave;
+    private JTextField svar;
 
 
 
     public Hovedvindu()  		//konstrukt?r uten parametre
     {
-//        super("Øveord");//super gj?r at denne subklassens konstrukt?r arver egenskapene til java-klassen Object.
+        super("Øveord");//super gj?r at denne subklassens konstrukt?r arver egenskapene til java-klassen Object.
 
-        kurs = new JButton("Kurs"+ "\n"); 		//oppretter knappene Kurs, Student, Oversikt og Avslutt
-        kurs.addActionListener(this);
-        student = new JButton("Student\t\t");
-        student.addActionListener(this);
-        oversikt = new JButton("Oversikt\t\t");
-        oversikt.addActionListener(this);
-        fag = new JButton("Fag\t\t");
-        fag.addActionListener(this);
+
+        laerer = new JButton("lærer\t\t");
+        laerer.addActionListener(this);
+        elev = new JButton("elev\t\t");
+        elev.addActionListener(this);
+
         avslutt = new JButton("Avslutt\t\t");
         avslutt.addActionListener(this);
-        utskriftsområde = new JTextField (12);
-        utskriftsområde.setSize( 300, 300 ); 	//kaller p? metoden setSize og setter st?rrelsen p? vinduet.
-        utskriftsområde.addActionListener(this);
+
+
 
         Container c = getContentPane(); 		//oppretter vinduets komponent-"container"
         c.setBackground(Color.WHITE);
         c.setLayout(new FlowLayout());  		//setter komponentene i "containeren" i riktig rekkef?lge
-        c.add(kurs);
-        c.add(student);
-        c.add(oversikt);
-        c.add(fag);
+        c.add(laerer);
+        c.add(elev);
+        c.add(besvart);
         c.add(avslutt);
+        c.add(oppgave);
+        c.add(svar);
     }
 
 
-    public void startStudent() 		  //her hentes Studentklassen.
+    public void startelev() 		  //her hentes Studentklassen.
     {
         Hovedvindu vindu = new Hovedvindu();
 		vindu.setSize( 700, 630 );
@@ -50,30 +49,34 @@ public class Hovedvindu extends JFrame implements ActionListener  //deklarerer d
 
     public void actionPerformed(ActionEvent e)  //Ved trykk kalles metodene opp.
     {
-    if (e.getSource() == kurs)
-        startKurs();
-    else if (e.getSource() == student)
-        startStudent();
-    else if (e.getSource() == oversikt)
-        startOversiktsplan();
-    else if (e.getSource() == fag)
-        startFag();
+    if (e.getSource() == elev)
+        startelev();
+    else if (e.getSource() == laerer)
+        startLaerer();
+    else if (e.getSource() == besvart)
+        startbesvart();
+    else if (e.getSource() == avslutt)
+        startavslutt();
     }
 
 
-	private void startFag() {
+	private void startLaerer() {
+		// TODO Auto-generated method stub
+
+		Hovedvindu vindu = new Hovedvindu();
+		vindu.setSize( 700, 630 );
+		vindu.setVisible( true );
+
+	}
+
+
+	private void startbesvart() {
 		// TODO Auto-generated method stub
 
 	}
 
 
-	private void startOversiktsplan() {
-		// TODO Auto-generated method stub
-
-	}
-
-
-	private void startKurs() {
+	private void startavslutt() {
 		// TODO Auto-generated method stub
 
 	}
