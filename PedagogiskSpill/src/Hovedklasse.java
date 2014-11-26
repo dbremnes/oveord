9a5d83a136021d644c7bdba31ba3f7ffa7c0c871
dbremnes <dbremnes@yahoo.no>
2014-11-26 09:33:50 +0100

    Update Hovedklasse.java
    Litt små endringer her bare

--- PedagogiskSpill/src/Hovedklasse.java
+++ PedagogiskSpill/src/Hovedklasse.java
@@ -11,31 +11,34 @@ public class Hovedklasse extends JFrame
         String [] verdier = {"Elev", "Lærer"};
         String valgt;
         String fil="laerersord.txt";
+        //Dette valget og if'en kan nå fjernes da menyen komme opp i hovedvinduet
+        //Trenger da bare innholdet i if'en
+
         valgt=(String)JOptionPane.showInputDialog(null, "Velg rolle", "Elev eller lærer?", JOptionPane.INFORMATION_MESSAGE, null, verdier, verdier[0] );
-        
+
         if (valgt.equals("Elev")){
         	Hovedvindu vindu = new Hovedvindu();
             vindu.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
             vindu.setSize(1000, 800);
             vindu.setVisible(true);
             Oppgavesamling oppgavesamlingen;
-            oppgavesamlingen = new Oppgavesamling(fil,1);
+            oppgavesamlingen = new Oppgavesamling(fil,1, vindu);
         }
         else {
-        	LaerersInnlegging laerer=new LaerersInnlegging();
-        
+        	Laerersinnlegging laerer=new Laerersinnlegging();
+
         }
-        	
+
         	//Hvis rollevalg er lærer
         //<start med å lese inn fra input/fil>
         //<metode?>
-        
+
         //Hvis ikke
         //<åpner hovedvindu med oppgavesamling>
         //OBS! Hvordan linker jeg "OK"-knapp sammen med åpning av hovedvindu?
-                
+
         //Konstruktør. Lager og viser fram hovedvindu
 
-        
+
     }
 }
