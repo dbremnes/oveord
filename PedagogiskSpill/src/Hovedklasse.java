c30daca9c2762226323349e417e41cfd6fc18da1
Sigrun <sigrun.lindaas@gmail.com>
2014-12-04 19:01:26 +0100

    Update Hovedklasse.java

--- PedagogiskSpill/src/Hovedklasse.java
+++ PedagogiskSpill/src/Hovedklasse.java
@@ -13,36 +13,41 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
 import java.util.*;
+/*
+ * Fyljande import skal brukast til å gi Tab-order, altså at input-feltet er første tab-stop, svar-knappen er nr.2 etc.
+ * import org.eclipse.wb.swing.FocusTraversalOnArray;
+ * Desverre funkar den ikkje!  Grrr...
+ */
 
-/* */    
+
 public class Hovedklasse extends JFrame {
 	private Scanner elevFil;
 	public Spilleren spiller;
 	
     public static void main (String[] args){
   
-    	//Programmet starter med � vente p� input fra bruker om rolle
-        String [] verdier = {"Elev", "L�rer"};
+    	//Programmet starter med å vente på input fra bruker om rolle
+        String [] verdier = {"Elev", "Lærer"};
         String valgt;
         String filnavn = "laerersord.txt";
-        valgt=(String)JOptionPane.showInputDialog(null, "Velg rolle", "Elev eller l�rer?", JOptionPane.INFORMATION_MESSAGE, null, verdier, verdier[0] );
+        valgt=(String)JOptionPane.showInputDialog(null, "Velg rolle", "Elev eller lærer?", JOptionPane.INFORMATION_MESSAGE, null, verdier, verdier[0] );
         
         if (valgt.equals("Elev")){
         	Hovedklasse vindu = new Hovedklasse();
             vindu.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-            vindu.setSize(1000, 800);
+            vindu.setSize(670, 750);
             
-            // Her kan vi vise vindu for � velge spiller.
+            // Her kan vi vise vindu for å velge spiller.
             vindu.velgSpiller();
             
             Oppgavesamling oppgavesamlingen;
             oppgavesamlingen = new Oppgavesamling(filnavn, vindu);
             vindu.setVisible(true);
         } else {
-        	// Kommentere, deklamere, reparere
+        	// Kommentere, deklamere, reparere, programmere!
         	LaerersInnlegging laerer=new LaerersInnlegging();       
         }	
-    }
+    } // end main
     
     public void velgSpiller(){
   
@@ -54,7 +59,7 @@ public class Hovedklasse extends JFrame {
 		Spilleren[] elevlisten = new Spilleren[100];		
 		try {
 	        this.elevFil=new Scanner(new File(filNavn));
-			 // Hentar inn "innlesteOppgaver.txt" (el.l.), som Kristina produserer i sin klasse, dette må vel utvides og endres med varierende oppgaver
+			 // Hentar inn tekstfilen som Kristina produserer i sin klasse
 		 } // end try
 	 	catch ( FileNotFoundException fileNotFoundException ){
 				Oppgavesamling.lagFil(filNavn,"");
@@ -62,17 +67,16 @@ public class Hovedklasse extends JFrame {
 		} // end catch
 		
 		while(this.elevFil.hasNext()){
-			 // instansier eit oppgave-objekt, i oppg-matrisen. Får objektet med data frå input-fila.
+			 // instansier eit oppgave-objekt, i oppg-matrisen. For objektet med data fra input-fila.
 				String fornavn = this.elevFil.next();
 				String etternavn = this.elevFil.next();
 				int nivaa = Integer.parseInt(this.elevFil.next());	
-				
+				// Puttar inn eit spelar-objekt i matrisen, som vi så brukar til å velje aktiv spelar. 
 				elevlisten[teller] = new Spilleren(nivaa,fornavn,etternavn);
 		}
 			//variabler for kontroll av andel korrekte svar
-	    
+	    // Matrisen returnerer valgt spillar objekt, som vi dermed gir peikaren "this.spiller". 
 	    this.spiller = (Spilleren) JOptionPane.showInputDialog(null, "Elevliste", "Velg navnet ditt", JOptionPane.INFORMATION_MESSAGE, null, elevlisten, elevlisten[0] ); 
-		JOptionPane.showMessageDialog(null,  "Niv�:"+this.spiller.getNivaa(), null, JOptionPane.PLAIN_MESSAGE );
-    }
-
-}
+		JOptionPane.showMessageDialog(null,  "Nivå:"+this.spiller.getNivaa(), null, JOptionPane.PLAIN_MESSAGE );
+    } // End velgSpiller()
+} // end Hovedklasse
