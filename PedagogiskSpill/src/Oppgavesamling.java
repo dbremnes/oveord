2aa103a3dd069cfe69f0a7648057fe738e82d3b5
Sigrun <sigrun.lindaas@gmail.com>
2014-12-04 10:30:52 +0100

    Update Oppgavesamling.java

--- PedagogiskSpill/src/Oppgavesamling.java
+++ PedagogiskSpill/src/Oppgavesamling.java
@@ -1,5 +1,5 @@
 //Oppgavesamling
-/* Denne klassa skal instansiere fleire objekt av typen â€œoppgaveâ€�, altsÃ¥ eit sett med oppgÃ¥ver (eller ei samling om du vil). Desse kan sÃ¥ testast og vil etterkvart innehalde sine poengsummar. Denne klassa vil dermed kunne innehalde aggregert poengsum og kommunisere med brukar-objektet om Ã¥ oppgradere level, tilfÃ¸re nye poeng (prosentar kanskje?)  */
+/* Denne klassa skal instansiere fleire objekt av typen â€œoppgaveâ€?, altså eit sett med oppgåver (eller ei samling om du vil). Desse kan så testast og vil etterkvart innehalde sine poengsummar. Denne klassa vil dermed kunne innehalde aggregert poengsum og kommunisere med brukar-objektet om å oppgradere level, tilføre nye poeng (prosentar kanskje?)  */
 
 import java.awt.Panel;
 import java.awt.Window;
@@ -7,8 +7,18 @@ import java.util.Scanner;
 import java.math.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.lang.IllegalStateException;
 import java.util.NoSuchElementException;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Formatter;
+
 
 
 import javax.swing.JOptionPane;
@@ -16,44 +26,45 @@ import javax.swing.JOptionPane;
 public class Oppgavesamling {
 
 	private Scanner fil;
-	private OppgavetypeInput[] oppgaver = new OppgavetypeInput[100];
+	private OppgavetypeInput[] oppgaver;
 	private Panel grensesnitt;
-	int spillerNummer;
-	int spillerNivaa=1;
-	int aktivoppgave;
+	//private int spillerNivaa;
+	private int aktivOppgaveNR;
 	private Hovedklasse vindu;
+	private int antallOppgaver;
+	private int antallRiktigeSvar;
+	private Spilleren spiller;
 
-	//private int oppgaver_sorteringsliste[]; // Innheld sorteringsrekkefÃ¸lga, produsert av metode sorter() Ser ikke at denne trengs
+	//private int oppgaver_sorteringsliste[]; // Innheld sorteringsrekkefølga, produsert av metode sorter() Ser ikke at denne trengs
 
+	public Oppgavesamling(String filNavn, Hovedklasse vindu) {
+		// Konstruktøren
 
-	public Oppgavesamling(String filen, int spillerNR, Hovedklasse vindu) {
-		/*
-		 * Konstruktøren
-		 */
-		this.vindu = vindu; // Sett peikaren frå konstruktør-parameteret til klassevariabel. 
-		 //spillerNivaa=spiller.getNivaa(spillerNR);
-		//super(); //hva er dette?
-		// Muligens redundant, kan sikker hente ut filnavnet frÃ¥ this.fil, men tek med inntil vidare:
-		try
-		{
-        this.fil=new Scanner(new File(filen));
-		 // Hentar inn "innlesteOppgaver.txt" (el.l.), som Kristina produserer i sin klasse, dette mÃ¥ vel utvides og endres med varierende oppgaver
-		this.lesInnOppgaveObjekt();  // Lag objekt pÃ¥ bakgrunn av fila
-		this.sorterOppgaver("alfabetisk");  // Sorter dei
-		/*
-		 * "Alfabetisk" bÃ¸r helst velgast via lÃ¦rar si innlegging, eller om lÃ¦rar vil, vÃ¦re eit alternativ i oppgavesamling-grensesnittet.
-		 */
-		this.avgrensUtvalget((int) Math.random()*10);
-		/*
-		 * Avgrensing bÃ¸r vÃ¦re lagt inn av lÃ¦rar, eller kunne velgast av spelar, akkurat som sortering.
-		 */
+		this.vindu = vindu; // Sett peikaren frå konstruktør-parameteret til klassevariabel.
+		this.spiller=vindu.spiller; // lag peikar til spiller-objektet fra hovudvinduet
+		//spillerNivaa=spilleren.getNivaa();
+			JOptionPane.showMessageDialog(null,  "Nivå:"+this.spiller.getNivaa(), null, JOptionPane.PLAIN_MESSAGE );
+
+		try {
+	        this.fil=new Scanner(new File(filNavn));
+			 // Hentar inn "laerersord.txt" (el.l.), som Kristina produserer i sin klasse, dette må vel utvides og endres med varierende oppgaver
+	        this.oppgaver = new OppgavetypeInput[100];
+	        this.lesInnOppgaveObjekt();  // Lag objekt på bakgrunn av fila
+			this.sorterOppgaver("alfabetisk");  // Sorter dei
+			
+			 // "Alfabetisk" bør helst velgast via lærar si innlegging, eller om lærar vil, være eit alternativ i oppgavesamling-grensesnittet.
+			 
+			// this.avgrensUtvalget((int) Math.random()*10);
+			/*
+			 * Avgrensing bør være lagt inn av lærar, eller kunne velgast av spelar, akkurat som sortering.
+			 */
 		 } // end try
-		 	catch ( FileNotFoundException fileNotFoundException )
-		 		{
-		 			JOptionPane.showMessageDialog(null,  "Feil ved Ã¥pning av fila.", null, JOptionPane.PLAIN_MESSAGE );
-		 			return;
+	 	catch ( FileNotFoundException fileNotFoundException ){
+				lagFil(filNavn);
+	 			JOptionPane.showMessageDialog(null,  "Feil ved åpning av fila.", null, JOptionPane.PLAIN_MESSAGE );
+	 			return;
 		} // end catch
-		this.aktivoppgave=0;
+		this.aktivOppgaveNR=0;
 		this.visAktivOppgave();
 
 	}
@@ -61,19 +72,19 @@ public class Oppgavesamling {
 	private void lesInnOppgaveObjekt() {
 		int oppgaveTeller=0;
 		while(this.fil.hasNext()){
-		 // instansier eit oppgave-objekt, i oppg-matrisen. FÃ´r objektet med data frÃ¥ input-fila.
+		 // instansier eit oppgave-objekt, i oppg-matrisen. Får objektet med data frå input-fila.
 			String oppgaveOrd = this.fil.next();
 			String oppgaveFasit = this.fil.next();
 			int nivaa = Integer.parseInt(this.fil.next());
-			/* Debug for å sjå kva som vert lest fra fila:
-			System.out.println(oppgaveOrd+"."+oppgaveFasit+"."+nivaa+"."); */
-			// Legg faktisk oppgaveobjekt_type_input i arrayen. 
-			// This er lagt til, fordi oppgåva skal kunne trigge "neste oppgåve" her i oppgåvesamlinga.
-			this.oppgaver[oppgaveTeller] = new OppgavetypeInput(oppgaveOrd,oppgaveFasit,nivaa,this);
-			//oppgavene skal bare legges til om nivÃ¥et stemmer
-			//if(spillerNivaa==oppgaver[oppgaveTeller].getNivaa)
-			oppgaveTeller++;
+			//oppgavene skal bare legges til om nivået stemmer	
+			if(this.spiller.getNivaa()==nivaa){
+				// This er lagt til, fordi oppgåva skal kunne trigge "neste oppgåve" her i oppgåvesamlinga.
+				this.oppgaver[oppgaveTeller] = new OppgavetypeInput(oppgaveOrd,oppgaveFasit,nivaa,this);
+				oppgaveTeller++;
+			}
 		}
+		//variabler for kontroll av andel korrekte svar
+		antallOppgaver=oppgaveTeller;
 	}
 
 	private void sorterOppgaver(String type)
@@ -94,35 +105,137 @@ public class Oppgavesamling {
 					oppgaver[i]=oppgaver[k];
 					oppgaver[k]=oppgaver[this.oppgaver.length];
 				}
+				aktivOppgaveNR=0;
 				//break;
 			//case 	"alfabetisk":
 				// Sorter elementa i array alfabetisk.
 				//break;
 			//default:
-				// Sorter elementa slik dei vart skrive inn av lÃ¦raren, altsÃ¥ etter key i oppgave-arrayen.
+				// Sorter elementa slik dei vart skrive inn av læraren, altså etter key i oppgave-arrayen.
 		}
-
-	private void avgrensUtvalget(int antall){
-		// kutt arrayen etter "antall" element, altsÃ¥ skal vi ikkje nÃ¸dvendigvis vise alle oppgaver fra tekstfila
-		if (antall>this.oppgaver.length)
-		{
-			for (int i=antall; i==this.oppgaver.length-1; i++)
-			{
-				oppgaver[i]=null;
-			}
-		}
-	}
+	//Skal dette fjernes med basis i at vi nå kun viser oppgaver på et bestemt nivå?
+	//private void avgrensUtvalget(int antall){
+		// kutt arrayen etter "antall" element, altså skal vi ikkje nødvendigvis vise alle oppgaver fra tekstfila
+		//if (antall>this.oppgaver.length){
+			//for (int i=antall; i==this.oppgaver.length-1; i++){
+			//	oppgaver[i]=null;
+			//}
+		//}
+	//}
 
 	public void visAktivOppgave(){
 		/*
-		 *  Viser oppgåva som skal vere aktiv. 
+		 *  Viser oppgåva som skal vere aktiv.
 		 */
-			this.vindu.setContentPane( oppgaver[this.aktivoppgave].visOppgave() );
+		this.vindu.setContentPane( oppgaver[this.aktivOppgaveNR].visOppgave() );
+		this.vindu.validate();
 	}
+
 	public void nesteOppgave(){
-		// Denne vert trigga av oppgave-objektet, når neste oppgåve skal visast. 
-		this.aktivoppgave++;
-		this.visAktivOppgave();
-		
+		// Denne vert trigga av oppgave-objektet, når neste oppgåve skal visast.
+		//sjekker om alle oppgaver på nivået er løst
+		if(this.aktivOppgaveNR==this.antallOppgaver-1){
+			this.antallRiktigeSvar=0;
+			for(int i=1;i>this.oppgaver.length;i++){
+				if(this.oppgaver[i].getRiktigBesvart()) this.antallRiktigeSvar++; 
+			}
+			//finner prosentandel riktige svar
+			double andel=this.antallRiktigeSvar/antallOppgaver;
+			//endrer nivået og oppgave utvalg om nivået er over 80%
+			if (andel>0.8){
+				int nyttNivaa = this.spiller.nivaaOpp();
+				//hva skal skje i tillegg?
+				//noe kult må skje
+				//dreper de gamle oppgavene
+				for(int i=0; i<antallOppgaver;i++){
+					this.oppgaver[i]=null;
+				}//end for
+				//sjekk om alt er riktig
+				if(andel==1){
+					//noe ekstra kult skal skje
+				}//end if
+
+			}
+			// Kva skal skje når spelet er over. 
+			JOptionPane.showMessageDialog(null, "", "Takk for alt!", JOptionPane.PLAIN_MESSAGE );
+			
+			// Køyre metode for å loope gjennom oppgavene og skriv ut i resultat-fil .
+			
+			vindu.dispose();
+	}else{
+		//henter frem oppgavene igjen
+			// this.lesInnOppgaveObjekt();
+			// this.sorterOppgaver("alfabetisk");
+			this.aktivOppgaveNR++;
+			this.visAktivOppgave();
 	}
 }
+
+	public static void lagFil(String filNavn)
+	{
+			Formatter output = null;
+			try
+			{
+			    //exception handling left as an exercise for the reader
+				FileWriter fileWriter = new FileWriter(filNavn, true);
+				output = new Formatter (fileWriter);//Ã‚pner fila for skriving, hvis den ikke finnes, opprettes den
+			}
+			catch ( SecurityException securityException )
+			{
+				JOptionPane.showMessageDialog(null, "", "Du har ikke skriverettigheter til denne fila", JOptionPane.PLAIN_MESSAGE );
+				System.exit( 1 ); // avslutter programmet
+
+			}
+			catch ( FileNotFoundException fileNotFoundException )
+			{
+				JOptionPane.showMessageDialog(null, "", "Feil ved Ã‚pning av fila", JOptionPane.PLAIN_MESSAGE );
+				System.exit( 1 ); // avslutter programmet
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+
+			// sopprett filen
+
+			System.out.println("");
+			System.out.println("");
+			System.out.println(0);
+			output.format("%s %s %d\n", "ny", "new", 0);
+
+			if ( output != null)
+				output.close();
+
+	}
+
+	public int getAntallRiktigeSvar() {
+		return this.antallRiktigeSvar;
+	}
+
+	public void setAntallRiktigeSvar(int antallRiktigeSvar) {
+		this.antallRiktigeSvar = antallRiktigeSvar;
+	}
+
+	public int getAktivOppgaveNR() {
+		return aktivOppgaveNR;
+	}
+
+	public void setAktivOppgaveNR(int aktivOppgaveNR) {
+		this.aktivOppgaveNR = aktivOppgaveNR;
+	}
+	
+}
+/*
+ *
+ * 	public int countLines(String filename) throws IOException {
+
+	    LineNumberReader reader  = new LineNumberReader(new FileReader(filename));
+	int cnt = 0;
+	String lineRead = "";
+	while ((lineRead = reader.readLine()) != null) {}
+
+	cnt = reader.getLineNumber();
+	reader.close();
+	return cnt;
+	}
+  */
